be8e5a1d882c3baa2e6b3850a00fcd527d2ac32e
Wenyu Gu <guwenyu61@gmail.com>
2024-06-24 23:27:30 -0400

    add cache support. fix the inconsistent vendor issue.

--- src/services/ipService.ts
+++ src/services/ipService.ts
@@ -1,14 +1,29 @@
 import IpVendorIpInfo from "../vendors/ipVendorIpInfo";
 import config from "../config/config";
+import NodeCache from "node-cache";
+import IpVendor from "../vendors/ipVendor";
 
 class IpService {
+  private cache: NodeCache;
+  private ipVendor: IpVendor;
+
+  constructor() {
+    this.cache = new NodeCache({ stdTTL: config.cacheTTL });
+    this.ipVendor = new IpVendorIpInfo(config.vendors.ipinfo);
+  }
+
   async getCountryByIp(ip: string): Promise<string> {
-    const ipVendor = new IpVendorIpInfo(config.vendors.ipinfo);
-    const country = ipVendor.getCountry(ip);
-    ipVendor.decrementRateLimit();
+    if (this.cache.has(ip)) {
+      console.log("Fetching the country from the cache...");
+      return this.cache.get(ip)!;
+    }
+
+    const country = this.ipVendor.getCountry(ip);
+    this.ipVendor.decrementRateLimit();
     console.log(
-      `Remaining requests for ${ipVendor.name}: ${ipVendor.remainingRequests}`
+      `Remaining requests for ${this.ipVendor.name}: ${this.ipVendor.remainingRequests}`
     );
+    this.cache.set(ip, country);
     return country;
   }
 }
