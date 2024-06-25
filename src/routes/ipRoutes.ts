459a56807f6f42e1f613e2c7ef85497336f534e5
Wenyu Gu <guwenyu61@gmail.com>
2024-06-25 00:40:13 -0400

    add tests.

--- src/routes/ipRoutes.ts
+++ src/routes/ipRoutes.ts
@@ -5,4 +5,8 @@ const ipRouter = Router();
 
 ipRouter.get("/country/:ip", (req, res) => ipController.getCountry(req, res));
 
+ipRouter.get("/country/", (req, res) => {
+  res.status(400).json({ error: "IP address is missing." });
+});
+
 export default ipRouter;
